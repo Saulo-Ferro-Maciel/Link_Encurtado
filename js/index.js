7d6093a506c63d53a18a51f22f76088e0f375120
Saulo Ferro Maciel <102975817+Saulo-Ferro-Maciel@users.noreply.github.com>
2023-07-12 18:19:38 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,51 @@
+function encurtarLink(){
+    let url =document.getElementById('input-url').value;
+    if(!url){
+        alert("Por favor, coloque um link válido!");
+        return;
+    }
+    else if(url.search('rebrand.ly') !== -1){
+        alert("Por favor, coloque um link válido!");
+        return;
+    }
+    else{
+        let header = {
+            "Content-Type": "application/json",
+            "apiKey": "63ad6f1c36d34ac98fdb03c5f96e7ba1"
+        }
+    
+        let linkRequest ={
+            destination: url,
+            domain: {fullName: "rebrand.ly"}
+        }
+
+        fetch("https://api.rebrandly.com/v1/links",{
+            method: "POST",
+            headers: header,
+            body: JSON.stringify(linkRequest)
+        })
+            .then(reponse => reponse.json())
+            .then(json => {
+                let inputUrl = document.getElementById('input-url');
+                inputUrl.value = json.shortUrl;
+            });
+    }
+    
+}
+
+function copiarLink() {
+    let inputUrl = document.getElementById('input-url');
+    let url =document.getElementById('input-url').value;
+
+    if(!url){
+        alert("Por favor, coloque um link válido!");
+        return;
+    } else{
+        inputUrl.select();
+        inputUrl.setSelectionRange(0, 99999);
+
+        document.execCommand('copy');
+
+        alert(`Link Copiado Com Sucesso: ${inputUrl.value}`);
+    }
+}
